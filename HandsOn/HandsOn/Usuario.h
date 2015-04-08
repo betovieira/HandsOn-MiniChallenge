5f65c50622b53b6ecbf5993bec75522424cbb3e4
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-04-08 14:33:13 -0300

    Commit para preparação de classes

---
+++ HandsOn/HandsOn/Usuario.h
@@ -0,0 +1,21 @@
+//
+//  Usuario.h
+//  HandsOn
+//
+//  Created by Humberto Vieira de Castro on 4/8/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Usuario : NSObject
+
+@property (nonatomic) int id_usuario;
+@property (nonatomic) NSString *nome;
+@property (nonatomic) NSString *email;
+@property (nonatomic) int id_usuario;
+@property (nonatomic) int id_usuario;
+@property (nonatomic) int id_usuario;
+
+
+@end
---
+++ HandsOn/HandsOn/Usuario.m
@@ -0,0 +1,13 @@
+//
+//  Usuario.m
+//  HandsOn
+//
+//  Created by Humberto Vieira de Castro on 4/8/15.
+//  Copyright (c) 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+#import "Usuario.h"
+
+@implementation Usuario
+
+@end
