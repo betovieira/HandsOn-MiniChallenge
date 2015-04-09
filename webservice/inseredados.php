a1a5e6240afa9934a909625e4f602c3efb60fde6
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-04-09 07:30:20 -0300

    Inseridos os comandos de retorno em JSON e Inserção de dados
    - Falta o teste
    - Funcoes de update de curtidas

--- webservice/inseredados.php
+++ webservice/inseredados.php
@@ -1,22 +1,81 @@
 <?php
 	include 'funcoes-acesso-banco.php';
-	$tipo_operacao1 = $_GET['tipo_operacao'];
+	// $tipo_operacao1 = $_GET['tipo_operacao'];
 	$tipo_operacao = $_POST['tipo_operacao'];
 
+	/*Cria o usuário */
 	if($tipo_operacao == 1)
 	{
-		$user = $_POST['user'];
-		$senha = $_POST['senha'];
-		$apelido = $_POST['apelido'];
+		$nomeUsuario = $_POST['nomeUsuario'];
 		$email = $_POST['email'];
+		$senha = $_POST['senha'];
+		$curso = $_POST['curso'];
+		$interesses = $_POST['interesses'];
 
-		$command = "INSERT INTO `usuario`(`id_usuario`, `user`, `senha`, `apelido`, `email`) VALUES (null,'". $user. "','". $senha ."','".$apelido."','". $email ."')";	
-		 
+		$command = "INSERT INTO `usuario`(`id_usuario`, `nome_usuario`, `email`, `senha`, `curso`, `interesses`) VALUES (null,'$nomeUsuario','$email','$senha','$curso','$interesses')";
+
+		insertMySQL($command);
+	} 
+	/* Insere Problema */
+	else if($tipo_operacao == 2) {
+		$id_usuario = $_POST['id_usuario'];
+		$id_area = $_POST['id_area'];
+		$descricaoProblema = $_POST['descricaoProblema'];
+		$curtidasProblema = $_POST['curtidasProblema'];
+
+		$command = "INSERT INTO `problema`(`id_problema`, `id_usuario`, `id_area`, `descricaoProblema`, `curtidasProblema`) VALUES (null,$id_usuario,$id_area,'$descricaoProblema',$curtidasProblema)";
+
 		insertMySQL($command);
 	}
+	/* UPDATE - Insere curtida de um problema */
+	else if($tipo_operacao == 3)
+	{
+
+	}
+	/* Insere soluções */
+	else if($tipo_operacao == 4)
+	{
+		$id_problema = $_POST['id_problema'];
+		$descricaoSolucao = $_POST['descricaoSolucao'];
+		$caminhoAnexoSolucao = $_POST['caminhoAnexoSolucao'];
+		$interesse = $_POST['interesse'];
+		$curtidasSolucao = $_POST['curtidasSolucao'];
+
+		$command = "INSERT INTO `solucao`(`id_solucao`, `id_problema`, `descricaoSolucao`, `caminhoAnexoSolucao`, `interesse`, `curtidasSolucao`) VALUES (null,$id_problema,'$descricaoSolucao', '$caminhoAnexoSolucao', '$interesse',$curtidasSolucao)";
+
+		insertMySQL($command);
+	} 
+	/* Insere curtida de uma solução */
+	else if($tipo_operacao == 5)
+	{
+
+	}
+	/* Insere implementação */
+	else if($tipo_operacao == 6)
+	{
+		$id_solucao = $_POST['id_solucao'];
+		$id_usuario = $_POST['id_usuario'];
+		$descricaoImplementacao = $_POST['descricaoImplementacao'];
+		$caminhoAnexoImplementacao = $_POST['caminhoAnexoImplementacao'];
+		$tags = $_POST['tags'];
+		$curtidasImplementacao = $_POST['curtidasImplementacao'];
+	
+		$command = "INSERT INTO `implementacao`(`id_implementacao`, `id_solucao`, `id_usuario`, `descricaoImplementacao`, `caminhoAnexoImplementacao`, `tags`, `curtidasImplementacao`) VALUES (null, $id_solucao, $id_usuario,'$descricaoImplementacao','$caminhoAnexoImplementacao','$tags',$curtidasImplementacao)"
+		insertMySQL($command);
+	}
+	/* Insere implementação */
+	else if($tipo_operacao == 7)
+	{
+
+	}
+
+/*
+	UPLOAD DE ARQUIVO
 	else if($tipo_operacao1 == 2)
 	{
 		echo 'entra';
 		uploadFile();
-	}
+	}*/
 ?>
+
+
--- webservice/retornadados.php
+++ webservice/retornadados.php
@@ -2,17 +2,143 @@
 	include 'funcoes-acesso-banco.php';
 
 	$tipo_operacao = $_GET['tipo_operacao'];
-
+	
+	/* RETORNA O USUÁRIO, SE ELE EXISTE */
 	if($tipo_operacao == 1)
 	{
-		$query = "SELECT * FROM `usuario`";
+		$email = $_GET['email'];
+		$senha = $_GET['senha'];
+
+		$query = "SELECT * FROM `usuario` WHERE email = '$email' and senha = '$senha'";
 		echo selectMySQL($query);
 	}
+	/* RETORNA USUÁRIOS POR ID_USUARIO*/
 	else if($tipo_operacao == 2)
 	{
-		$user = $_GET['user'];
+		$id_usuario = $_GET['id_usuario'];
 
-		$query = "SELECT * FROM `usuario` WHERE `user` like '%" . $user. "%'";
+		$query = "SELECT * FROM `usuario` WHERE id_usuario = $id_usuario";
+		echo selectMySQL($query);
+	}
+	/* RETORNA TODAS AS ÁREAS */
+	else if($tipo_operacao == 3)
+	{
+		$query = "SELECT * FROM `area`";
+		echo selectMySQL($query);
+	}
+	/* RETORNA TODOS OS PROBLEMAS */
+	else if($tipo_operacao == 4)
+	{
+		$query = "SELECT * FROM `problema` ";
+		echo selectMySQL($query);
+	}
+	/* RETORNA TODOS OS PROBLEMAS POR AREA */
+	else if($tipo_operacao == 5)
+	{
+		$id_area = $_GET['id_area'];
+		$query = "SELECT * FROM `problema` WHERE id_area = $id_area ";
+		echo selectMySQL($query);
+	}
+	/* RETORNA TODOS OS PROBLEMAS POR AREA */
+	else if($tipo_operacao == 6)
+	{
+		$id_area = $_GET['id_area'];
+		$query = "SELECT * FROM `problema` WHERE id_area = $id_area ";
+		echo selectMySQL($query);
+	} 
+	/* RETORNA POR NUMERO DE CURTIDA */
+	else if($tipo_operacao == 7)
+	{
+		$query = "SELECT * FROM `problema` ORDER BY curtidasProblema DESC";
+		echo selectMySQL($query);
+	}
+	/* RETORNAR A QUANTIDADE DE SOLUCOES PARA UM PROBLEMA */
+	else if($tipo_operacao == 8)
+	{
+		$id_problema = $_GET['id_problema'];
+		$query = "SELECT COUNT(*) FROM `solucao` WHERE id_problema = $id_problema";
+
+		echo selectMySQL($query);
+	}	
+	/* Retorna todas as soluções por problema */
+	else if($tipo_operacao == 9)
+	{
+		$id_problema = $_GET['id_problema'];
+		$query = "SELECT * FROM `solucao` WHERE id_problema = $id_problema";
+
+		echo selectMySQL($query);
+	}
+	/* 
+		------------------------------------------------------------------------------
+		SOLUCOES
+	*/
+	/* RETORNA AS SOLUCOES */
+	else if($tipo_operacao == 10)
+	{
+		$query = "SELECT * FROM `solucao`";
+		echo selectMySQL($query);
+	}
+	/* RETORNA AS SOLUCOES POR ID_SOLUCOES */
+	else if($tipo_operacao == 11)
+	{
+		$id_solucao = $_GET['id_solucao'];
+		$query = "SELECT * FROM `solucao` WHERE id_solucao = $id_solucao";
+		echo selectMySQL($query);
+	}
+	/* RETORNA TODOS OS SOLUCOES POR AREA */
+	else if($tipo_operacao == 12)
+	{
+		$id_area = $_GET['id_area'];
+		$query = "SELECT * FROM `solucao` WHERE id_area = $id_area ";
+		echo selectMySQL($query);
+	}
+	/* RETORNA SOLUCOES POR NUMERO DE CURTIDA */
+	else if($tipo_operacao == 13)
+	{
+		$query = "SELECT * FROM `solucao` ORDER BY curtidasSolucao DESC";
+		echo selectMySQL($query);
+	}
+	/* Retornar a quantidade de implementações para uma solução*/
+	else if($tipo_operacao == 14)
+	{
+		$id_solucao = $_GET['id_solucao'];
+		$query = "SELECT COUNT(*) FROM `implementacao` WHERE id_solucao = $id_solucao";
+
+		echo selectMySQL($query);
+	}	
+	/* Retornar todas as implementacoes por soluções */
+	else if($tipo_operacao == 15)
+	{
+		$id_problema = $_GET['id_solucao'];
+		$query = "SELECT * FROM `implementacao` WHERE id_solucao = $id_solucao";
+
+		echo selectMySQL($query);
+	}
+	/*
+		-----------------------IMPLEMENTACAO---------------------------
+		Retorna as implementações 
+	*/
+	else if($tipo_operacao == 15)
+	{
+		$query = "SELECT * FROM `implementacao` ";
+		echo selectMySQL($query);
+	}
+	/*	Retorna as implementações por área
+	*/
+	else if($tipo_operacao == 16)
+	{
+		$id_area = $_GET['id_area'];
+		$query = "SELECT * FROM `implementacao` where id_area = $id_area ";
+		echo selectMySQL($query);
+	}
+	/* Retorna as implementações por numero de curtidas e mais recentes */
+	else if($tipo_operacao == 17) {
+		$query = "SELECT * FROM `implementacao`  ORDER BY curtidasImplementacao DESC";
+		echo selectMySQL($query);
+	}
+	/* Retorna as implementações por numero de curtidas e mais recentes */
+	else if($tipo_operacao == 18) {
+		$query = "SELECT * FROM `implementacao`  ORDER BY curtidasImplementacao DESC";
 		echo selectMySQL($query);
 	}
 
